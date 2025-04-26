353a9946165e0d84afc96ce392c34e6da199762b
AngelPenaGarcia05 <angelpenaGar@hotmail.com>
2025-04-26 17:27:11 -0500

    feat(common): agregar configuraciones generales de la aplicacion
    Se añaden ApplicationConfig, CorsConfig, EndpointPaths y ScheduledConfig para centralizar parametros comunes.
    
    Esto facilita la gestion y mantenimiento de configuraciones compartidas en toda la aplicacion.

---
+++ src/main/java/com/clinicavillegas/app/common/ScheduledConfig.java
@@ -0,0 +1,35 @@
+package com.clinicavillegas.app.common;
+
+import com.clinicavillegas.app.appointment.models.Cita;
+import com.clinicavillegas.app.appointment.repositories.CitaRepository;
+import com.clinicavillegas.app.appointment.specifications.CitaSpecification;
+import com.clinicavillegas.app.email.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@EnableScheduling
+public class ScheduledConfig {
+
+    @Autowired
+    private CitaRepository citaRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void executeTask() {
+        Specification<Cita> specs = CitaSpecification.conFecha(LocalDateTime.now().toLocalDate().plusDays(1))
+                .and(CitaSpecification.conEstado("Pendiente"));
+        List<Cita> citas = citaRepository.findAll(specs);
+        for (Cita cita : citas) {
+            emailService.enviarRecordatorio(cita);
+        }
+    }
+}
